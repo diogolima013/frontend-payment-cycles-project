ade412a98555a23212c0ad57fc4b4a3372073cb3
Diogo <diogoslva0133@gmail.com>
2022-11-24 07:50:26 -0300

    Displaying BillingCycleList data on screen

--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -7,7 +7,17 @@ class BillingCycleList extends Component {
 
     componentWillMount() {
         this.props.getList()
-        
+    }
+
+    renderRows() {
+        const list = this.props.list || []
+        return list.map (bc => (
+            <tr key={bc._id}>
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ))
     }
 
     render() {
@@ -23,7 +33,7 @@ class BillingCycleList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-
+                        {this.renderRows()}
                     </tbody>
                 </table>
             </div>
